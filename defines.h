ec7d15c28f6f268975a5d49642dbfe62ef92f753
Johan Lanzrein <johan.lanzrein@epfl.ch>
2018-01-19 13:51:17 -0600

    Format as requested by the lab

---
+++ defines.h
@@ -0,0 +1,28 @@
+/**
+ * @file defines.h
+ * @brief file that contains a few structures and function to help the parsing of defines
+ * */
+ 
+ #define MAX_DEFINES 4096
+ 
+ typedef struct {
+	 char* identifier
+	 char* arbitraryText
+ }define;
+ 
+ //~ typedef struct node{
+	//~ define* def;
+	//~ struct node* next;
+ 
+ //~ };
+ 
+ //~ int createNode(node* newNode,define* data, node* next);
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
